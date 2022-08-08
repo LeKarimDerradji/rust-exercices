6881b33f1fda5aa2710890b9495c2813ac49ff41
LeKarimDerradji <karim.derradji.pro@gmail.com>
2022-08-09 01:12:48 +0200

    a5 done

--- exercises/activities/src/bin/a5.rs
+++ exercises/activities/src/bin/a5.rs
@@ -9,4 +9,23 @@
 // * Print the variable within the loop statement
 // * Use break to exit the loop
 
-fn main() {}
+fn main() {
+    // Solution 1
+    let mut int = 1;
+    loop {
+        println!("int is equal to: {:?}", int);
+        if int < 4 {
+            int += 1;
+        } else {
+            break;
+        }
+    }
+
+    // Solution 2 with while loop
+    let mut another_int = 1;
+
+    while another_int < 5 {
+        println!("int is equal to: {:?}", another_int);
+        another_int += 1;
+    }
+}
