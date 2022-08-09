eb74a6bfd8d40067354e5d9f432b82fd3ffb56e6
LeKarimDerradji <karim.derradji.pro@gmail.com>
2022-08-09 02:34:43 +0200

    a7

--- exercises/activities/src/bin/a7.rs
+++ exercises/activities/src/bin/a7.rs
@@ -9,5 +9,25 @@
 // * The function must use the enum as a parameter
 // * Use a match expression to determine which color
 //   name to print
+enum Colors {
+    Blue,
+    Red,
+    Green,
+    Purple,
+}
 
-fn main() {}
+fn print_color(color: Colors) {
+    match color {
+        Colors::Blue => println!("The color is blue"),
+        Colors::Red => println!("The color is red"),
+        Colors::Green => println!("The color is green"),
+        Colors::Purple => println!("The color is purple"),
+    }
+}
+
+fn main() {
+    print_color(Colors::Blue);
+    print_color(Colors::Red);
+    print_color(Colors::Green);
+    print_color(Colors::Purple);
+}
