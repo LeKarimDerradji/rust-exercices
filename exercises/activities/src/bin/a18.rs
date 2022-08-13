5899e6c6ce0863a8aaea8af8e964d30fb6960c57
LeKarimDerradji <karim.derradji.pro@gmail.com>
2022-08-13 23:59:50 +0200

    result

--- exercises/activities/src/bin/a18.rs
+++ exercises/activities/src/bin/a18.rs
@@ -10,5 +10,25 @@
 // * Use a function to determine if a customer can make a restricted purchase
 // * Return a result from the function
 // * The Err variant should detail the reason why they cannot make a purchase
+struct Customer {
+    id: i32,
+    age: i32,
+    country: String,
+}
 
-fn main() {}
+impl Customer {
+    fn allowed(&self) -> Result<String> {
+        if self.age >= 21 {
+            Ok("User able to make that purchase".to_owned()),
+        } else {
+            Err("User is unable to make that purchase".to_owned())
+        }
+    }
+}
+
+fn main() {
+    let customer_1 = Customer{id: 1, age: 18, country: "EN".to_owned()};
+    let customer_2 = Customer{id: 2, age: 21, country: "ES".to_owned()};
+
+
+}
