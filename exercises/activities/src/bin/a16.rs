efd1fcd202df3f40188053f7a7c8a3897ed76999
LeKarimDerradji <karim.derradji.pro@gmail.com>
2022-08-13 18:51:31 +0200

    Option<T>

--- exercises/activities/src/bin/a16.rs
+++ exercises/activities/src/bin/a16.rs
@@ -11,4 +11,21 @@
 // * Use a struct containing the student's name and locker assignment
 // * The locker assignment should use an Option<i32>
 
-fn main() {}
+struct LockersData {
+    name: String,
+    locker_assigned: Option<i32>,
+}
+
+impl LockersData {
+    fn print_datas(&self) {
+        println!("{} got a locker assigned which is: {:?}", self.name, self.locker_assigned)
+    }
+}
+
+fn main() {
+    let locker_one = LockersData{name: "Karim".to_owned(), locker_assigned: Some(32)};
+    let locker_two = LockersData{name: "Eve".to_owned(), locker_assigned: None};
+
+    locker_one.print_datas();
+    locker_two.print_datas();
+}
