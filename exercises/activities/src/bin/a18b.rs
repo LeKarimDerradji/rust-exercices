93a4d96cf6a10734f21f999d30096251c7900a5e
LeKarimDerradji <karim.derradji.pro@gmail.com>
2022-08-14 15:14:13 +0200

    err_propagation

--- exercises/activities/src/bin/a18b.rs
+++ exercises/activities/src/bin/a18b.rs
@@ -27,14 +27,76 @@ enum Position {
     Manager,
     LineSupervisor,
     KitchenStaff,
-    AssemblyTechnician,
+    AssemblyTech,
+}
+
+enum Status {
+    Active,
+    Terminated,
 }
 
 struct Employee {
     position: Position,
-    employed: Bool,
+    status: Status,
+}
+
+fn try_access(employee: &Employee) -> Result<(), String> {
+    match employee.status {
+        Status::Terminated => return Err("Access restricted: employee is terminated!".to_owned()),
+        _ => (),
+    }
+
+    match employee.position {
+        Position::Maintenance => Ok(()),
+        Position::Marketing => Ok(()),
+        Position::Manager => Ok(()),
+        _ => Err("Access restricted: invalid position!".to_owned()),
+    }
+}
+
+fn print_access(employee: &Employee) -> Result<(), String> {
+    let _attempt_to_access = try_access(employee)?;
+    println!("Employee is authorized!");
+    Ok(())
 }
 
 fn main() {
-    
+    let employees = vec![
+        Employee {
+            position: Position::KitchenStaff,
+            status: Status::Active,
+        },
+        Employee {
+            position: Position::Manager,
+            status: Status::Active,
+        },
+        Employee {
+            position: Position::Marketing,
+            status: Status::Active,
+        },
+        Employee {
+            position: Position::Maintenance,
+            status: Status::Terminated,
+        },
+        Employee {
+            position: Position::LineSupervisor,
+            status: Status::Active,
+        },
+        Employee {
+            position: Position::AssemblyTech,
+            status: Status::Active,
+        },
+    ];
+
+    // Err
+    // Ok
+    // Ok
+    // Err
+
+    for employee in employees {
+        match print_access(&employee) {
+            Err(e) => println!("{}", e),
+            _ => (),
+        }
+    }
 }
